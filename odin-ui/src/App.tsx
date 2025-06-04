2ef28a33e1d17c9cebb9ca1ba75b26f7b0923e86
Thanh Bui <thanh.v.bui511@gmail.com>
2025-06-04 17:23:14 +0300

    add mapbox and fetch png overlay

--- odin-ui/src/App.tsx
+++ odin-ui/src/App.tsx
@@ -1,19 +1,56 @@
 import './App.css';
-
-const ErrorMessage = () => {
-  return (
-      <div className="error-message-container">
-        <h1 className="error-message">
-        ERROR 404 - ODIN NOT FOUND
-        </h1>
-      </div>
-  );
-};
-
+import mapboxgl from 'mapbox-gl'
+import 'mapbox-gl/dist/mapbox-gl.css';
+import {useEffect} from "react";
 
 export const App = (): JSX.Element => {
-  return (
-    <ErrorMessage/>
-  );
+
+    useEffect(() => {
+        mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
+        const mapbox = new mapboxgl.Map({
+            container: "map-container",
+            center: [22.4673, 59.8613],
+            zoom: 12,
+        });
+
+        mapbox.on('load', async () => {
+            let coords: [[number, number], [number, number], [number, number], [number, number]] | undefined;
+            try {
+
+                const response = await fetch("http://localhost:8000/sar_coords");
+                if (!response.ok) {
+                    throw new Error(`${response.status}`);
+                }
+                coords = await response.json();
+            } catch (e) {
+                console.error("Failed to fetch SAR coordinates: ", e);
+            }
+
+            if(coords) {
+                mapbox.addSource('sar', {
+                    'type': 'image',
+                    'url': 'http://localhost:8000/sar_image',
+                    'coordinates': coords
+                });
+                mapbox.addLayer({
+                    id: 'sar-layer',
+                    'type': 'raster',
+                    'source': 'sar',
+                    'paint': {
+                        'raster-opacity': 1
+                    }
+                });
+            }
+        });
+        return () => {
+            mapbox.remove()
+        }
+    }, [])
+
+    return (
+        <>
+            <div id="map-container"></div>
+        </>
+    )
 }
 
